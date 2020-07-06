7c7f5ddf002c8e0026ec94ce4bf09fb2d06efe27
jian.xu <jian.xu@dena.com>
2020-07-06 19:20:26 +0800

    Add Constructing a Fractal

---
+++ Assets/4.Constructing a Fractal/Fractal.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+    public int maxDepth = 4;
+    public float childScale;
+
+    private int depth = 0;
+
+    private void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+
+        if(depth < maxDepth)
+        {
+            //new GameObject("Fractal Child").AddComponent<Fractal>();
+            StartCoroutine(CreateChildren());            
+        }
+    }
+
+    private IEnumerator CreateChildren()
+    {
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up);
+
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right);
+
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.left);
+    }
+
+    private void Initialize(Fractal partent, Vector3 direction)
+    {
+        mesh = partent.mesh;
+        material = partent.material;
+        maxDepth = partent.maxDepth;
+        depth = partent.depth + 1;
+        childScale = partent.childScale;
+        transform.parent = partent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = direction * (0.5f + 0.5f * childScale);
+    }
+}
