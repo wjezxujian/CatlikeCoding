0ccc61cba5530342a91352ac58dacf065eeabc0d
jian.xu <jian.xu@dena.com>
2020-07-07 19:25:58 +0800

    Add 5.Frames Per Seconds

---
+++ Assets/5.Frames Per Seconds/NucleonSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour
+{
+    public float timeBetweenSpawns;
+    public float spawnDistance;
+    public Nucleon[] nucleonPrefabs;
+
+    private float timeSinceLastSpawn;
+
+    private void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if(timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    private void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
