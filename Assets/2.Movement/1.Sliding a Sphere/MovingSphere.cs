450ccbc81a96535ec049836db49a90ca8343edfc
jian.xu <jian.xu@dena.com>
2020-07-10 18:58:56 +0800

    Update 2.1 Sliding a Sphere

--- Assets/2.Movement/1.Sliding a Sphere/MovingSphere.cs
+++ Assets/2.Movement/1.Sliding a Sphere/MovingSphere.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
+    [SerializeField, Range(0, 100f)]
+    float maxSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,19 @@ public class MovingSphere : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+
+        //playerInput.Normalize();
+        //playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        //transform.localPosition = new Vector3(playerInput.x, 0f, playerInput.y);
+
+        //Vector3 displacement = new Vector3(playerInput.x, 0f, playerInput.y);
+        //transform.localPosition += displacement;
+
+        Vector3 velocity = new Vector3(playerInput.x, 0f, playerInput.y);
+        Vector3 displacement = velocity * maxSpeed * Time.deltaTime;
+        transform.localPosition += displacement;
     }
 }
