680d7346a1e4836487979802c0c2ab9edd204f70
jian.xu <jian.xu@dena.com>
2020-08-21 18:19:36 +0800

    update 2.4.Movement_OrbitCamera

--- Assets/2.Movement/3.Surface Contact/MovingSphere3.cs
+++ Assets/2.Movement/3.Surface Contact/MovingSphere3.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public class MovingSphere3 : MonoBehaviour
 {
+    [SerializeField]
+    Transform playerInputSpace = default;
+
     [SerializeField, Range(0, 100f)]
     float maxSpeed = 10f;
 
@@ -103,11 +106,31 @@ public class MovingSphere3 : MonoBehaviour
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
 
         //控制速度，时间决定增加速度
         //Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        if (playerInputSpace)
+        {
+        
+            Vector3 right = playerInputSpace.right;
+            right.y = 0;
+            right.Normalize();
+
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
+
+            //desiredVelocity = playerInputSpace.TransformDirection(playerInput.x, 0f, playerInput.y) * maxSpeed;
+            desiredVelocity = (right * playerInput.x + forward * playerInput.y) * maxSpeed;
+        }
+        else
+        {
+            desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        }
+
+        
 
         //velocity = body.velocity;
         //float maxSpeedChange = maxAcceleration * Time.deltaTime;
--- Assets/2.Movement/4.OrbitCamera/OrbitCamera.cs
+++ Assets/2.Movement/4.OrbitCamera/OrbitCamera.cs
@@ -18,13 +18,43 @@ public class OrbitCamera : MonoBehaviour
     [SerializeField, Range(1f, 360)]
     float rotationSpeed = 90f;
 
-    Vector3 focusPoint;
+    [SerializeField, Range(-89f, 89f)]
+    float minVerticalAngle = -30f, maxVerticalAngle = 60f;
+
+    [SerializeField, Min(0f)]
+    float alignDelay = 5f;      //延时自动对齐时间
+
+    [SerializeField, Range(0f, 90f)]
+    float alignSmoothRange = 45f;
+
+    [SerializeField]
+    LayerMask obstructionMask = -1;
+
+    Vector3 focusPoint, previousFocusPoint;
 
     Vector2 orbitAngles = new Vector2(45f, 0f);
 
+    float lastManualRotationTime;
+
+    Camera regularCamera;
+
+    Vector3 CameraHalfExtends
+    {
+        get
+        {
+            Vector3 halfExtends;
+            halfExtends.y = regularCamera.nearClipPlane * Mathf.Tan(0.5f * Mathf.Deg2Rad * regularCamera.fieldOfView);
+            halfExtends.x = halfExtends.y * regularCamera.aspect;
+            halfExtends.z = 0;
+            return halfExtends;
+        }
+    }
+
     private void Awake()
     {
+        regularCamera = GetComponent<Camera>();
         focusPoint = focus.position;
+        transform.localRotation = Quaternion.Euler(orbitAngles);
     }
 
     // Start is called before the first frame update
@@ -41,19 +71,68 @@ public class OrbitCamera : MonoBehaviour
 
     private void LateUpdate()
     {
-        //Vector3 focusPoint = focus.position;
+        ////2.2 控制轨道
+        ////Vector3 focusPoint = focus.position;
+        //UpdateFocusPoint();
+        //ManualRotation();
+        //Quaternion lookRotation = Quaternion.Euler(orbitAngles);
+        ////Vector3 lookDirection = transform.forward;
+        //Vector3 lookDirection = lookRotation * Vector3.forward;
+        ////transform.localPosition = focusPoint - lookDirection * distance;
+        //Vector3 lookPosition = focusPoint - lookDirection * distance;
+        //transform.SetPositionAndRotation(lookPosition, lookRotation);
+
+        //2.3 约束角度
         UpdateFocusPoint();
-        ManualRotation();
-        Quaternion lookRotation = Quaternion.Euler(orbitAngles);
-        //Vector3 lookDirection = transform.forward;
+        Quaternion lookRotation;
+        if (ManualRotation() || AutomaticRotation())
+        {
+            ConstrainAngles();
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }
+        else
+        {
+            lookRotation = transform.localRotation;
+        }
+
         Vector3 lookDirection = lookRotation * Vector3.forward;
-        //transform.localPosition = focusPoint - lookDirection * distance;
         Vector3 lookPosition = focusPoint - lookDirection * distance;
+
+        ////4.1减少外观距离 
+        //if (Physics.Raycast(focusPoint, -lookDirection, out RaycastHit hit, distance))
+        //{
+        //    lookPosition = focusPoint - lookDirection * hit.distance;
+        //}
+
+        ////4.2保持近平面清晰 采用盒子投射
+        //if (Physics.BoxCast(focusPoint, CameraHalfExtends, -lookDirection, out RaycastHit hit, lookRotation, distance - regularCamera.nearClipPlane))
+        //{
+        //    lookPosition = focusPoint - lookDirection * (hit.distance + regularCamera.nearClipPlane);
+        //}
+
+        //4.3聚焦半径
+        Vector3 rectOffset = lookDirection * regularCamera.nearClipPlane;
+        Vector3 rectPosition = lookPosition + rectOffset;
+        Vector3 castFrom = focus.position;
+        Vector3 castLine = rectPosition - castFrom;
+        float castDistance = castLine.magnitude;
+        Vector3 castDirection = castLine / castDistance;
+
+        if(Physics.BoxCast(castFrom, CameraHalfExtends, castDirection, out RaycastHit hit, lookRotation, castDistance, obstructionMask))
+        {
+            //lookPosition = focusPoint - lookDirection * (hit.distance + regularCamera.nearClipPlane);
+            rectPosition = castFrom + castDirection * hit.distance;
+            lookPosition = rectPosition - rectOffset;
+        }
+
+
         transform.SetPositionAndRotation(lookPosition, lookRotation);
     }
 
     private void UpdateFocusPoint()
     {
+        previousFocusPoint = focusPoint;
+
         Vector3 targetPoint = focus.position;
 
         if(focusRadius > 0)
@@ -80,7 +159,7 @@ public class OrbitCamera : MonoBehaviour
         }               
     }
 
-    private void ManualRotation()
+    private bool ManualRotation()
     {
         Vector2 input = new Vector2(Input.GetAxis("Vertical Camera"), Input.GetAxis("Horizontal Camera"));
 
@@ -88,6 +167,72 @@ public class OrbitCamera : MonoBehaviour
         if(input.x < -e || input.x > e || input.y < -e || input.y > e)
         {
             orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+            lastManualRotationTime = Time.unscaledTime;
+            return true;
         }
+
+        return false;
+    }
+
+    private bool AutomaticRotation()
+    {
+        if(Time.unscaledTime - lastManualRotationTime < alignDelay)
+        {
+            return false;
+        }
+
+        Vector2 movement = new Vector2(focusPoint.x - previousFocusPoint.x, focusPoint.z - previousFocusPoint.z);
+        float movementDelataSqr = movement.sqrMagnitude;
+        if(movementDelataSqr < 0.000001f)
+        {
+            return false;
+        }
+
+        float headingAngle = GetAngle(movement / Mathf.Sqrt(movementDelataSqr));
+        float deltaAbs = Mathf.Abs(Mathf.DeltaAngle(orbitAngles.y, headingAngle));
+        float rotationChange = rotationSpeed * Mathf.Min(Time.unscaledDeltaTime, movementDelataSqr);
+        
+        if(deltaAbs < alignSmoothRange)
+        {
+            rotationChange *= deltaAbs / alignSmoothRange;
+        }else if(180f - deltaAbs < alignSmoothRange)
+        {
+            rotationChange *= (180 - deltaAbs) / alignSmoothRange;
+        }
+
+        orbitAngles.y = Mathf.MoveTowardsAngle(orbitAngles.y, headingAngle, rotationChange);
+
+        return true;
+    }
+
+    private void OnValidate()
+    {
+        if(maxVerticalAngle < minVerticalAngle)
+        {
+            maxVerticalAngle = minVerticalAngle;
+        }
+    }
+
+    private void ConstrainAngles()
+    {
+        orbitAngles.x = Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+
+        if(orbitAngles.y < 0f)
+        {
+            orbitAngles.y += 360f;
+        }
+        else if(orbitAngles.y > 360)
+        {
+            orbitAngles.y -= 360;
+        }
+
+    }
+
+    
+
+    static float GetAngle(Vector2 direction)
+    {
+        float angle = Mathf.Acos(direction.y) * Mathf.Rad2Deg;
+        return direction.x < 0f ? 360f - angle : angle;
     }
 }
