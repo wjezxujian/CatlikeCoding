ee22cf0769539f23c768700823622f68751f7431
jian.xu <jian.xu@dena.com>
2020-08-31 19:19:14 +0800

    Add 2.5.Custom Gravity

---
+++ Assets/2.Movement/5.Custom Gravity/CustomGravity.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CustomGravity : MonoBehaviour
+{
+    public static Vector3 GetGravity(Vector3 position)
+    {
+        //return Physics.gravity;
+        return position.normalized * Physics.gravity.y;
+    }
+
+    public static Vector3 GetUpAxis(Vector3 position)
+    {
+        //return -Physics.gravity.normalized;
+        //return position.normalized;
+        Vector3 up = position.normalized;
+        return Physics.gravity.y < 0f ? up : -up;
+    }
+    
+    public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis)
+    {
+        //upAxis = -Physics.gravity.normalized;
+        //return Physics.gravity;
+        Vector3 up = position.normalized;
+        upAxis = Physics.gravity.y < 0f ? up : -up;
+        return up * Physics.gravity.y;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+}
---
+++ Assets/2.Movement/5.Custom Gravity/CustomGravityRigidbody.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CustomGravityRigidbody : MonoBehaviour
+{
+    [SerializeField]
+    bool floatToSleep = false;
+
+    Rigidbody body;
+
+    float floatDelay;
+
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        body.useGravity = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (floatToSleep)
+        {
+            if (body.IsSleeping())
+            {
+                floatDelay = 0f;
+                return;
+            }
+
+            if (body.velocity.sqrMagnitude < 0.0001f)
+            {
+                floatDelay += Time.deltaTime;
+                if (floatDelay >= 1f)
+                {
+                    return;
+                }
+            }
+        }
+
+        
+
+        body.AddForce(CustomGravity.GetGravity(body.position), ForceMode.Acceleration);    
+    }
+}
