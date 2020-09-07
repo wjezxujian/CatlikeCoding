f4211f074fc377e587aceb0ef36e3f3d3f908479
jian.xu <wjezxujian@gmail.com>
2020-09-07 23:50:33 +0800

    Update 2.6.Complex Gravity Box

--- Assets/2.Movement/6.Complex Gravity/GravityBox.cs
+++ Assets/2.Movement/6.Complex Gravity/GravityBox.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
 
 public class GravityBox : GravitySource
 {
@@ -22,28 +23,80 @@ public class GravityBox : GravitySource
         position = transform.InverseTransformDirection(position - transform.position);
 
         Vector3 vector = Vector3.zero;
-        Vector3 distance;
-        distance.x = boundaryDistance.x - Mathf.Abs(position.x);
-        distance.y = boundaryDistance.y - Mathf.Abs(position.y);
-        distance.z = boundaryDistance.z - Mathf.Abs(position.z);
-        if(distance.x < distance.y)
+        int outside = 0;
+        if(position.x > boundaryDistance.x)
         {
-            if(distance.x < distance.z)
+            vector.x = boundaryDistance.x - position.x;
+            outside = 1;
+        }
+        else if (position.x < -boundaryDistance.x)
+        {
+            vector.x = -boundaryDistance.x - position.x;
+            outside = 1;
+        }
+
+        if (position.y > boundaryDistance.y)
+        {
+            vector.y = boundaryDistance.y - position.y;
+            outside += 1;
+        }
+        else if (position.y < -boundaryDistance.y)
+        {
+            vector.y = -boundaryDistance.y - position.y;
+            outside += 1;
+        }
+
+        if (position.z > boundaryDistance.z)
+        {
+            vector.z = boundaryDistance.z - position.z;
+            outside += 1;
+        }
+        else if (position.z < -boundaryDistance.z)
+        {
+            vector.z = -boundaryDistance.z - position.z;
+            outside += 1;
+        }
+
+        if (outside > 0)
+        {
+            float distance = outside == 1 ? Mathf.Abs(vector.x + vector.y + vector.z) : vector.magnitude;
+            if (distance > outerFalloffDistance)
             {
-                vector.x = GetGravityComponent(position.x, distance.x);
+                return Vector3.zero;
+            }
+
+            float g = gravity / distance;
+            if(distance > outerDistance)
+            {
+                g *= 1f - (distance - outerDistance) * outerFalloffFactor;
+            }
+            
+            return transform.TransformDirection(g * vector);
+        }
+
+
+        Vector3 distances;
+        distances.x = boundaryDistance.x - Mathf.Abs(position.x);
+        distances.y = boundaryDistance.y - Mathf.Abs(position.y);
+        distances.z = boundaryDistance.z - Mathf.Abs(position.z);
+        if(distances.x < distances.y)
+        {
+            if(distances.x < distances.z)
+            {
+                vector.x = GetGravityComponent(position.x, distances.x);
             }
             else
             {
-                vector.z = GetGravityComponent(position.z, distance.z);
+                vector.z = GetGravityComponent(position.z, distances.z);
             }
         }
-        else if(distance.y < distance.z)
+        else if(distances.y < distances.z)
         {
-            vector.y = GetGravityComponent(position.y, distance.y);
+            vector.y = GetGravityComponent(position.y, distances.y);
         }
         else
         {
-            vector.z = GetGravityComponent(position.z, distance.z);
+            vector.z = GetGravityComponent(position.z, distances.z);
         }
 
         return transform.TransformDirection(vector);
@@ -77,9 +130,9 @@ public class GravityBox : GravitySource
         float maxInner = Mathf.Min(Mathf.Min(boundaryDistance.x, boundaryDistance.y), boundaryDistance.z);
         innerDistance = Mathf.Min(innerDistance, maxInner);
         innerFalloffDistance = Mathf.Max(Mathf.Min(innerFalloffDistance, maxInner), innerDistance);
-        outerFalloffDistance = Mathf.Max(outerFalloffFactor, outerDistance);
+        outerFalloffDistance = Mathf.Max(outerFalloffDistance, outerDistance);
 
-        innerFalloffFactor = 1f / (innerFalloffFactor - innerDistance);
+        innerFalloffFactor = 1f / (innerFalloffDistance - innerDistance);
         outerFalloffFactor = 1f / (outerFalloffDistance - outerDistance);
     }
 
@@ -108,7 +161,67 @@ public class GravityBox : GravitySource
 
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(Vector3.zero, 2f * boundaryDistance);
+
+
+        if(outerDistance > 0f)
+        {
+            Gizmos.color = Color.yellow;
+            DrawGizmosOuterCube(outerDistance);
+        }
+
+        if(outerFalloffDistance > outerDistance)
+        {
+            Gizmos.color = Color.cyan;
+            DrawGizmosOuterCube(outerFalloffDistance);
+        }
     }
 
+    void DrawGizomsRect(Vector3 a, Vector3 b, Vector3 c, Vector3 d)
+    {
+        Gizmos.DrawLine(a, b);
+        Gizmos.DrawLine(b, c);
+        Gizmos.DrawLine(c, d);
+        Gizmos.DrawLine(d, a);
+    }
+
+    void DrawGizmosOuterCube(float distance)
+    {
+        Vector3 a, b, c, d;
+        a.y = b.y = boundaryDistance.y;
+        d.y = c.y = -boundaryDistance.y;
+        b.z = c.z = boundaryDistance.z;
+        d.z = a.z = -boundaryDistance.z;
+        a.x = b.x = c.x = d.x = boundaryDistance.x + distance;
+        DrawGizomsRect(a, b, c, d);
+        a.x = b.x = c.x = d.x = -a.x;
+        DrawGizomsRect(a, b, c, d);
+
+        a.x = d.x = boundaryDistance.x;
+        b.x = c.x = -boundaryDistance.x;
+        a.z = b.z = boundaryDistance.z;
+        c.z = d.z = -boundaryDistance.z;
+        a.y = b.y = c.y = d.y = boundaryDistance.y + distance;
+        DrawGizomsRect(a, b, c, d);
+        a.y = b.y = c.y = d.y = -a.y;
+        DrawGizomsRect(a, b, c, d);
+
+        a.x = d.x = boundaryDistance.x;
+        b.x = c.x = -boundaryDistance.x;
+        a.y = b.y = boundaryDistance.y;
+        c.y = d.y = -boundaryDistance.y;
+        a.z = b.z = c.z = d.z = boundaryDistance.z + distance;
+        DrawGizomsRect(a, b, c, d);
+        a.z = b.z = c.z = d.z = -a.z;
+        DrawGizomsRect(a, b, c, d);
+
+        distance *= 0.5773502692f;
+        Vector3 size = boundaryDistance;
+        size.x = 2f * (size.x + distance);
+        size.y = 2f * (size.y + distance);
+        size.z = 2f * (size.z + distance);
+        Gizmos.DrawWireCube(Vector3.zero, size);
+    }
+
+    
     
 }
