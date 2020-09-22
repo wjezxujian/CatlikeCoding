fd6565efb574ec23ec6aaac93c1a6054b56d65cf
jian.xu <wjezxujian@gmail.com>
2020-09-22 23:27:48 +0800

    Add 3.1.Object Management,Persisting Objects

---
+++ 3.Object Management/1.Persisting Objects/Assets/Scenes/Game.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public Transform prefab;
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+
+    List<Transform> objects;
+
+    void Awake()
+    {
+        objects = new List<Transform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(createKey))
+        {
+            //Instantiate(prefab);
+            CreateObject();
+        }else if (Input.GetKey(newGameKey))
+        {
+            BeginNewGame();
+        }
+    }
+
+    void CreateObject()
+    {
+        Transform t = Instantiate(prefab);
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        objects.Add(t);
+    }
+
+    void BeginNewGame()
+    {
+        for(int i = 0; i < objects.Count; ++i)
+        {
+            Destroy(objects[i].gameObject);
+        }
+        objects.Clear();
+    }
+}
