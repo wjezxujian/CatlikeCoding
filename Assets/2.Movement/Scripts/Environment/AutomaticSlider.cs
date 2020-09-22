3eaf5d8c156c3783a7b003a6f57bf250c8deed12
jian.xu <jian.xu@dena.com>
2020-09-22 17:13:26 +0800

    Update 2.10.Reactive Environment

---
+++ Assets/2.Movement/Scripts/Environment/AutomaticSlider.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AutomaticSlider : MonoBehaviour
+{
+    [SerializeField, Min(0.01f)]
+    float duration = 1f;
+
+    [SerializeField]
+    bool autoReverse = false, smoothstep = false;
+
+    [System.Serializable]
+    public class OnValueChangedEvent : UnityEvent<float> { }
+
+    [SerializeField]
+    //UnityEvent<float> onValueChanged = default;
+    OnValueChangedEvent onValueChanged = default;
+
+    public bool Reversed { get; set; }
+
+    public bool AutoReverse
+    {
+        get => autoReverse;
+        set => autoReverse = value;
+    }
+
+    float value;
+
+    //bool reversed;
+
+    float SmoothedValue => 3f * value * value - 2f * value * value * value;
+
+    private void FixedUpdate()
+    {
+        float delta = Time.deltaTime / duration;
+        if (Reversed)
+        {
+            value -= delta;
+            if(value <= 0f)
+            {
+                if (autoReverse)
+                {
+                    value = Mathf.Min(1f, -value);
+                    Reversed = false;
+                }
+                else
+                {
+                    value = 0f;
+                    enabled = false;
+                }
+            }
+        }
+        else
+        {
+            value += delta;
+            if(value >= 1f)
+            {
+                if (autoReverse)
+                {
+                    value = Mathf.Max(0f, 2f - value);
+                    Reversed = true;
+                }
+                else
+                {
+                    value = 1f;
+                    enabled = false;
+                }
+            }
+        }
+
+        //onValueChanged.Invoke(value);
+        onValueChanged.Invoke(smoothstep ? SmoothedValue : value);
+    }
+}
