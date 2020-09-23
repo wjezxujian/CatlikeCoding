b4a11d71f5d4ad224ec3fa4bf6be27d9cfdaa64d
jian.xu <jian.xu@dena.com>
2020-09-23 16:41:01 +0800

    Add 3.3.Object Variety

---
+++ 3.Object Management/2.Object Variety/Assets/Scripts/PersistentStorage.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class PersistentStorage : MonoBehaviour
+{
+    string savePath;
+
+    private void Awake()
+    {
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+
+    public void Save(PersistableObject o)
+    {
+        using(
+            BinaryWriter writer = new BinaryWriter(File.Open(savePath, FileMode.Create))
+        ){
+            o.Save(new GameDataWriter(writer));
+        }
+    }
+
+    public void Load(PersistableObject o)
+    {
+        using (
+            BinaryReader reader = new BinaryReader(File.Open(savePath, FileMode.Open))
+        ){
+            o.Load(new GameDataReader(reader));
+        }
+    }
+}
