a0ceb83f72fdf25bbdabae2ff3149a5d8e7c5119
jian.xu <wjezxujian@gmail.com>
2020-09-24 23:35:53 +0800

    Add 3.7.Configuring Shape

---
+++ 3.Object Management/7.Configuring Shape/Assets/Scripts/CompositeSpawnZone.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class CompositeSpawnZone : SpawnZone
+{
+    [SerializeField]
+    SpawnZone[] spawnZones;
+
+    [SerializeField]
+    bool sequential;
+
+    int nextSequentialIndex = 0;
+
+    public override Vector3 SpawnPoint
+    {
+        get
+        {
+            int index;
+            if (sequential) {
+                index = nextSequentialIndex++;
+                if(nextSequentialIndex >= spawnZones.Length)
+                {
+                    nextSequentialIndex = 0;
+                }
+            }
+            else
+            {
+                index = Random.Range(0, spawnZones.Length);
+            }
+
+            return spawnZones[index].SpawnPoint;
+        }
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(nextSequentialIndex);
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        nextSequentialIndex = reader.ReaderInt();
+    }
+}
---
+++ 3.Object Management/7.Configuring Shape/Assets/Scripts/SpawnZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public abstract class SpawnZone : PersistableObject
+{
+    //[SerializeField]
+    //bool surfaceOnly;
+
+    //public Vector3 SpawnPoint
+    //{
+    //    get 
+    //    {
+    //        //return Random.insideUnitSphere * 5f + transform.position; 
+    //        return transform.TransformPoint(
+    //            surfaceOnly ? Random.onUnitSphere: Random.insideUnitSphere);
+    //    }
+    //}
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.cyan;
+    //    Gizmos.matrix = transform.localToWorldMatrix;
+    //    Gizmos.DrawWireSphere(Vector3.zero, 1f);
+    //}
+
+    public virtual Vector3 SpawnPoint { get; }
+}
