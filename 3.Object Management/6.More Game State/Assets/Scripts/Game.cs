75ac6b611d252c5319ede4ebf849b5ad93a6ef98
jian.xu <jian.xu@dena.com>
2020-09-24 17:25:50 +0800

    Add 3.6.More Game State

---
+++ 3.Object Management/6.More Game State/Assets/Scripts/Game.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : PersistableObject
+{
+    [SerializeField]
+    ShapeFactory shapeFactory;
+
+    [SerializeField]
+    KeyCode createKey = KeyCode.C, destroyKey = KeyCode.X,
+        newGameKey = KeyCode.N, saveKey = KeyCode.S, loadKey = KeyCode.L;
+
+    [SerializeField]
+    PersistentStorage storage;
+
+    [SerializeField]
+    int levelCount;
+
+    //public SpawnZone spawnZone;
+    public SpawnZone SpawnZoneOfLevel { get; set; }
+
+    public float CreationSpeed { get; set; }
+
+    public float DestructionSpeed { get; set; }
+
+    public static Game Instance { get; private set; }
+
+    const int saveVersion = 2;
+
+    List<Shape> shapes;
+
+    float creationProgress, destructionProgress;
+
+    int loadedLevelBuildIndex;
+
+
+    //void Awake()
+    //{
+    //    shapes = new List<Shape>();
+
+    //    Scene loadedLevel = SceneManager.GetSceneByName("Level 1");
+    //    if (loadedLevel.isLoaded)
+    //    {
+    //        SceneManager.SetActiveScene(loadedLevel);
+    //        return;
+    //    }
+
+    //    StartCoroutine(LoadLevel());
+    //}
+
+    private void Start()
+    {
+        Instance = this;
+
+        shapes = new List<Shape>();
+
+        if (Application.isEditor)
+        {
+            for(int i = 0; i < SceneManager.sceneCount; ++i)
+            {
+                Scene loadedLevel = SceneManager.GetSceneAt(i);
+                if (loadedLevel.name.Contains("Level "))
+                {
+                    SceneManager.SetActiveScene(loadedLevel);
+                    loadedLevelBuildIndex = loadedLevel.buildIndex;
+                    return;
+                }
+            }
+        }        
+
+        StartCoroutine(LoadLevel(1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(createKey))
+        {
+            CreateShape();
+        }
+        else if (Input.GetKeyDown(destroyKey))
+        {
+            DestroyShape();
+        }
+        else if (Input.GetKey(newGameKey))
+        {
+            BeginNewGame();
+        }
+        else if (Input.GetKeyDown(saveKey))
+        {
+            storage.Save(this, saveVersion);
+        }
+        else if (Input.GetKeyDown(loadKey))
+        {
+            BeginNewGame();
+            storage.Load(this);
+        }
+        else
+        {
+            for(int i = 0; i <= levelCount; ++i)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+                {
+                    BeginNewGame();
+                    StartCoroutine(LoadLevel(i));
+                }
+            }
+        }
+
+        creationProgress += Time.deltaTime * CreationSpeed;
+        while(creationProgress >= 1f)
+        {
+            creationProgress -= 1f;
+            CreateShape();
+        }
+
+        destructionProgress += Time.deltaTime * DestructionSpeed;
+        while(destructionProgress >= 1f)
+        {
+            destructionProgress -= 1f;
+            DestroyShape();
+        }
+    }
+
+    void OnEnable()
+    {
+        Instance = this;
+    }
+
+    void CreateShape()
+    {
+        Shape instance = shapeFactory.GetRandom();
+        Transform t = instance.transform;
+        //t.localPosition = Random.insideUnitSphere * 5f;
+        t.localPosition = SpawnZoneOfLevel.SpawnPoint;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        shapes.Add(instance);
+    }
+
+    void DestroyShape()
+    {
+        if (shapes.Count > 0)
+        {
+            int index = Random.Range(0, shapes.Count);
+            shapeFactory.Reclaim(shapes[index]);
+            int lastIndex = shapes.Count - 1;
+            shapes[index] = shapes[lastIndex];
+            shapes.RemoveAt(lastIndex);
+        } 
+    }
+
+    void BeginNewGame()
+    {
+        for(int i = 0; i < shapes.Count; ++i)
+        {
+            shapeFactory.Reclaim(shapes[i]);
+        }
+        shapes.Clear();
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(shapes.Count);
+        writer.Write(loadedLevelBuildIndex);
+        for(int i = 0; i < shapes.Count; ++i)
+        {
+            writer.Write(shapes[i].ShapeId);
+            writer.Write(shapes[i].MaterialId);
+            shapes[i].Save(writer);
+        }
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        int version = reader.Version;
+        if(version > saveVersion)
+        {
+            Debug.LogError("Unsupported future save version" + version);
+            return;
+        }
+
+        int count = version <= 0 ? -version : reader.ReaderInt();
+        StartCoroutine(LoadLevel(version < 2 ? 1 : reader.ReaderInt()));
+        for(int i = 0; i < count; ++i)
+        {
+            int shapeId = version > 0 ? reader.ReaderInt() : 0;
+            int materialId = version > 0 ? reader.ReaderInt() : 0;
+            Shape instance = shapeFactory.Get(shapeId, materialId);
+            instance.Load(reader);
+            shapes.Add(instance);
+        }
+    }
+
+
+    IEnumerator LoadLevel(int levelBuildIndex)
+    {
+        enabled = false;
+        //SceneManager.LoadScene("Level 1", LoadSceneMode.Additive);
+        //yield return null;
+        //异步加载
+        //yield return SceneManager.LoadSceneAsync("Level 1", LoadSceneMode.Additive);
+
+        if(loadedLevelBuildIndex > 0)
+        {
+            yield return SceneManager.UnloadSceneAsync(loadedLevelBuildIndex);
+        }
+
+        yield return SceneManager.LoadSceneAsync(levelBuildIndex, LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(levelBuildIndex));
+        loadedLevelBuildIndex = levelBuildIndex;
+        enabled = true;
+    }
+}
