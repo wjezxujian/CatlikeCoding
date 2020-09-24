c75160604a88dc9ff31f482b33c498eb5508b90d
jian.xu <jian.xu@dena.com>
2020-09-24 17:04:10 +0800

    Add 3.5.Spawn Zones

---
+++ 3.Object Management/5.Spawn Zones/Assets/Scripts/GameLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GameLevel : MonoBehaviour
+{
+    [SerializeField]
+    SpawnZone spawnZone;
+
+    void Start()
+    {
+        Game.Instance.SpawnZoneOfLevel = spawnZone;
+    }
+}
---
+++ 3.Object Management/5.Spawn Zones/Assets/Scripts/PersistentStorage.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class PersistentStorage : MonoBehaviour
+{
+    string savePath;
+
+    private void Awake()
+    {
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+
+    public void Save(PersistableObject o, int version)
+    {
+        using(
+            BinaryWriter writer = new BinaryWriter(File.Open(savePath, FileMode.Create))
+        ){
+            writer.Write(-version);
+            o.Save(new GameDataWriter(writer));
+        }
+    }
+
+    public void Load(PersistableObject o)
+    {
+        using (
+            BinaryReader reader = new BinaryReader(File.Open(savePath, FileMode.Open))
+        ){
+            o.Load(new GameDataReader(reader, -reader.ReadInt32()));
+        }
+    }
+}
