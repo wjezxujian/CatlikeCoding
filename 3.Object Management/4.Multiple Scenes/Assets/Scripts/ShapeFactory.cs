19433c4031ff1ffec66f0eb2d44aae42c179c00a
jian.xu <jian.xu@dena.com>
2020-09-24 11:07:46 +0800

    Add 3.4.Multiple Scenes

---
+++ 3.Object Management/4.Multiple Scenes/Assets/Scripts/ShapeFactory.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ShapeFactory : ScriptableObject
+{
+    [SerializeField]
+    Shape[] prefabs;
+
+    [SerializeField]
+    Material[] materials;
+
+    [SerializeField]
+    bool recycle;
+
+    List<Shape>[] pools;
+
+    public Shape Get(int shapeId = 0, int materialId = 0)
+    {
+        Shape instance;
+        if (recycle)
+        {
+            if(pools == null)
+            {
+                CreatePools();
+            }
+
+            List<Shape> pool = pools[shapeId];
+            int lastIndex = pool.Count - 1;
+            if(lastIndex >= 0)
+            {
+                instance = pool[lastIndex];
+                instance.gameObject.SetActive(true);
+                pool.RemoveAt(lastIndex);
+            }
+            else
+            {
+                instance = Instantiate(prefabs[shapeId]);
+                instance.ShapeId = shapeId;
+            }        
+        }
+        else
+        {
+            instance = Instantiate(prefabs[shapeId]);
+            instance.ShapeId = shapeId;
+        }
+
+        instance.SetMaterial(materials[materialId], materialId);
+        instance.SetColor(Random.ColorHSV(0f, 1f, 0.5f, 1f, 0.25f, 1f, 1f, 1f));
+        return instance;
+    }
+
+    public Shape GetRandom()
+    {
+        int shapeId = Random.Range(0, prefabs.Length);
+        int materialId = Random.Range(0, materials.Length);
+        return Get(shapeId, materialId);
+    }
+
+    public void Reclaim(Shape shapeToRecycle)
+    {
+        if (recycle)
+        {
+            if (pools == null)
+            {
+                CreatePools();
+            }
+
+            pools[shapeToRecycle.ShapeId].Add(shapeToRecycle);
+            shapeToRecycle.gameObject.SetActive(false);
+        }
+        else
+        {
+            Destroy(shapeToRecycle.gameObject);
+        }
+    }
+
+    private void CreatePools()
+    {
+        pools = new List<Shape>[prefabs.Length];
+        for(int i = 0; i < prefabs.Length; ++i)
+        {
+            pools[i] = new List<Shape>();
+        }    
+    }
+
+    
+}
