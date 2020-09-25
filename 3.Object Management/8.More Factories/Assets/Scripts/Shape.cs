be6b364b51146351ad37655dc70d901029d49d9a
jian.xu <jian.xu@dena.com>
2020-09-25 16:54:54 +0800

    Add 3.8.More Factories

---
+++ 3.Object Management/8.More Factories/Assets/Scripts/Shape.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : PersistableObject
+{
+    MeshRenderer meshRenderer;
+
+    int shapeId = int.MinValue;
+
+    Color color;
+
+    static int colorPropertyId = Shader.PropertyToID("_Color");
+    static MaterialPropertyBlock sharedPropertyBlock;
+
+    public int ShapeId
+    {
+        get { return shapeId; }
+        set 
+        { 
+            if(shapeId == int.MinValue && value != int.MinValue)
+            {
+                shapeId = value;
+            }
+            else
+            {
+                Debug.LogError("Not allowed to change shapeId");
+            }
+            
+        }
+    }
+
+    public int MaterialId
+    {
+        get;
+        private set;
+    }
+
+    public Vector3 AngularVelocity { get; set; }
+    public Vector3 Velocity { get; set; }
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    //private void FixedUpdate()
+    public void GameUpdate()
+    {
+        transform.Rotate(AngularVelocity * Time.deltaTime);
+        transform.localPosition += Velocity * Time.deltaTime;
+    }
+
+    public void SetMaterial(Material material, int materialId)
+    {
+        MaterialId = materialId;
+        meshRenderer.material = material;
+    }
+
+    public void SetColor(Color color)
+    {
+        this.color = color;
+        if(sharedPropertyBlock == null)
+        {
+            sharedPropertyBlock = new MaterialPropertyBlock();
+        }
+        sharedPropertyBlock.SetColor(colorPropertyId, color);
+        meshRenderer.SetPropertyBlock(sharedPropertyBlock);
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        base.Save(writer);
+        writer.Write(color);
+        writer.Write(AngularVelocity);
+        writer.Write(Velocity);
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        base.Load(reader);
+        SetColor(reader.Version > 0 ? reader.ReadColor() : Color.white);
+        AngularVelocity = reader.Version >= 4 ? reader.ReadVector3() : Vector3.zero;
+        Velocity = reader.Version >= 4 ? reader.ReadVector3() : Vector3.zero;
+    }
+
+
+
+
+}
---
+++ 3.Object Management/8.More Factories/Assets/Scripts/SpawnZone.cs
@@ -0,0 +1,64 @@
+﻿using UnityEditor.Build;
+using UnityEngine;
+
+public abstract class SpawnZone : PersistableObject
+{
+    [System.Serializable]
+    public class SpawnConfguration
+    {
+        public enum MovementDirection
+        {
+            Forward,
+            Upward,
+            Outward,
+            Random
+        }
+
+        public MovementDirection movementDirection;
+
+        public FloatRange speed;
+
+        public FloatRange angularSpeed;
+
+        public FloatRange scale;
+
+        public ColorRangeHSV color;
+    }
+
+    [SerializeField]
+    SpawnConfguration spawnConfg;
+
+    public virtual Vector3 SpawnPoint { get; }
+
+    public virtual void ConfigureSpawn(Shape shape)
+    {
+        Transform t = shape.transform;
+        t.localPosition = SpawnPoint;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * spawnConfg.scale.RandomValueInRange;
+        //shape.SetColor(Random.ColorHSV(hueMin: 0f, hueMax: 1f, saturationMin: 0.5f,
+        //    saturationMax: 1f, valueMin: 0.25f, valueMax: 1f, alphaMin: 1f, alphaMax: 1f));
+        shape.SetColor(spawnConfg.color.RandomInRange);
+        shape.AngularVelocity = Random.onUnitSphere * spawnConfg.scale.RandomValueInRange;
+        //shape.Velocity = Random.onUnitSphere * Random.Range(0f, 2f);
+
+        Vector3 direction;
+        switch (spawnConfg.movementDirection)
+        {
+            case SpawnConfguration.MovementDirection.Upward:
+                direction = transform.up;
+                break;
+            case SpawnConfguration.MovementDirection.Outward:
+                direction = (t.localPosition - transform.position).normalized;
+                break;
+            case SpawnConfguration.MovementDirection.Random:
+                direction = Random.onUnitSphere;
+                break;
+            default:
+                direction = transform.forward;
+                break;
+        }
+       
+        shape.Velocity = direction * spawnConfg.speed.RandomValueInRange;
+    }
+}
