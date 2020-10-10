a1f8d92774304d3b6a0f0d5acf58c2584b7511c4
jian.xu <jian.xu@dena.com>
2020-10-10 18:20:17 +0800

    Add 4.2.Enemies

---
+++ 4.Tower Defense/2.Enemies/Assets/Scripts/Game.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    Vector2Int boardSize = new Vector2Int(11, 11);
+
+    [SerializeField]
+    GameBoard board = default;
+
+    [SerializeField]
+    GameTileContentFactory tileContentFactory = default;
+
+    Ray TouchRay => Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    private void Awake()
+    {
+        board.Initialize(boardSize, tileContentFactory);
+        board.ShowGrid = true;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            HandleTouch();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            HandleAlternativeTouch();
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            board.ShowPaths = !board.ShowPaths;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            board.ShowGrid = !board.ShowGrid;
+        }
+    }
+
+    private void OnValidate()
+    {
+        if(boardSize.x < 2)
+        {
+            boardSize.x = 2;
+        }
+
+        if(boardSize.y < 2)
+        {
+            boardSize.y = 2;
+        }
+    }
+
+    private void HandleTouch()
+    {
+        GameTile tile = board.GetTile(TouchRay);
+        if(tile != null)
+        {
+            //tile.Content = tileContentFactory.Get(GameTileContentType.Destination);
+            board.ToggleDestination(tile);
+        }
+    }
+
+    private void HandleAlternativeTouch()
+    {
+        GameTile tile = board.GetTile(TouchRay);
+        if(tile != null)
+        {
+            board.ToggleWall(tile);
+        }
+    }
+}
---
+++ 4.Tower Defense/2.Enemies/Assets/Scripts/GameTileContent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameTileContent : MonoBehaviour
+{
+    [SerializeField]
+    GameTileContentType type = default;
+
+    GameTileContentFactory originFactory;
+
+    public GameTileContentType Type => type;
+
+    public GameTileContentFactory OriginFactory 
+    {
+        get => originFactory;
+        set
+        {
+            Debug.Assert(originFactory == null, "Redefined origin factory!");
+            originFactory = value;
+        }
+    }
+
+    public void Recyle()
+    {
+        originFactory.Reclaim(this);
+    }
+
+}
