a9408716e096d7eaaa33be9a4ef137a716841569
jian.xu <jian.xu@dena.com>
2020-10-22 16:52:09 +0800

    Update 6.1.Procedural Grid

---
+++ 6.Mesh Basics/1.Procedural Grid/Assets/Scripts/Grid.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Grid : MonoBehaviour
+{
+    public int xSize, ySize;
+
+    public Vector3[] vertices;
+
+    private Mesh mesh;
+
+    private void Awake()
+    {
+        StartCoroutine(Generate());
+    }
+
+    private IEnumerator Generate()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y < ySize; ++y)
+        {
+            for (int x = 0; x <= xSize; ++x, ++i)
+            {
+                vertices[i] = new Vector3(x, y);
+                yield return wait;
+            }
+
+        }
+
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[3];
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = xSize + 1;
+        mesh.triangles = triangles;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        for(int i = 0; i < vertices.Length; ++i)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+
+        }
+    }
+}
