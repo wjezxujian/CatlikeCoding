e36b678a5e9a350c61691da30764b3f98461a970
jian.xu <jian.xu@dena.com>
2020-10-23 14:26:51 +0800

    Update 6.1.Procedural Grid

--- 6.Mesh Basics/1.Procedural Grid/Assets/Scripts/Grid.cs
+++ 6.Mesh Basics/1.Procedural Grid/Assets/Scripts/Grid.cs
@@ -13,43 +13,63 @@ public class Grid : MonoBehaviour
 
     private void Awake()
     {
-        StartCoroutine(Generate());
+        //StartCoroutine(Generate());
+        Generate();
     }
 
-    private IEnumerator Generate()
+    //private IEnumerator Generate()
+    private void Generate()
     {
-        WaitForSeconds wait = new WaitForSeconds(0.05f);
+        //WaitForSeconds wait = new WaitForSeconds(0.02f);
 
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Procedural Grid";
 
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-        for (int i = 0, y = 0; y < ySize; ++y)
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0, 0f, -1f);
+        for (int i = 0, y = 0; y <= ySize; ++y)
         {
             for (int x = 0; x <= xSize; ++x, ++i)
             {
                 vertices[i] = new Vector3(x, y);
-                yield return wait;
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent;
+                //yield return wait;
             }
-
         }
 
         mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
 
-        int[] triangles = new int[3];
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = xSize + 1;
-        mesh.triangles = triangles;
+        int[] triangles = new int[xSize * ySize * 6];
+
+
+        for(int ti = 0, vi = 0, y = 0, z = 0; y < ySize; ++y, ++vi)
+        {
+            for(int x = 0; x < xSize; ++x, ti+=6, ++vi)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+                mesh.triangles = triangles;
+                mesh.RecalculateNormals();
+                //yield return wait;
+            }   
+        }
+        
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.black;
-        for(int i = 0; i < vertices.Length; ++i)
-        {
-            Gizmos.DrawSphere(vertices[i], 0.1f);
+        //Gizmos.color = Color.black;
+        //for(int i = 0; i < vertices.Length; ++i)
+        //{
+        //    Gizmos.DrawSphere(vertices[i], 0.1f);
 
-        }
+        //}
     }
 }
