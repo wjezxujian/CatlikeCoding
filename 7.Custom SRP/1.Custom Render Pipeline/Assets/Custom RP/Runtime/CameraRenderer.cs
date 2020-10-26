0531be74d839bdd21ce66cdf0fca294157b0c6c9
wjezxujian <wjezxujian@gmail.com>
2020-10-26 23:31:16 +0800

    Update 7.1.Custom Render Pipeline

---
+++ 7.Custom SRP/1.Custom Render Pipeline/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    private void DrawVisibleGeometry()
+    {
+        context.DrawSkybox(camera);
+    }
+
+    private void Submit()
+    {
+        context.Submit();
+    }
+}
