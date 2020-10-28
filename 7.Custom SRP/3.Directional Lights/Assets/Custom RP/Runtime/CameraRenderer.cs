fecaac24a519f7d440b672145ccab4888d0ca85a
jian.xu <jian.xu@dena.com>
2020-10-28 17:24:19 +0800

    Add 7.3.Directional Lights Base Project

---
+++ 7.Custom SRP/3.Directional Lights/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    const string bufferName = "Render Camera";
+
+    CommandBuffer buffer = new CommandBuffer { name = bufferName };
+
+    CullingResults cullingResults;
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+   
+
+    public void Render(ScriptableRenderContext context, Camera camera, bool useDynamicBatching, bool useGPUInstancing)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry(useDynamicBatching, useGPUInstancing);
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+
+    private void DrawVisibleGeometry(bool useDynamicBatching, bool useGPUInstancing)
+    {
+        SortingSettings sortingSettings = new SortingSettings(camera) { criteria = SortingCriteria.CommonOpaque};
+        DrawingSettings drawingSettrings = new DrawingSettings(unlitShaderTagId, sortingSettings) {
+            enableDynamicBatching = useDynamicBatching,
+            enableInstancing = useGPUInstancing
+        };
+        FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        context.DrawRenderers(cullingResults, ref drawingSettrings, ref filteringSettings);
+        context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettrings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults, ref drawingSettrings, ref filteringSettings);
+    }
+
+    private void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        buffer.ClearRenderTarget(flags <= CameraClearFlags.Depth, flags == CameraClearFlags.Color, 
+            flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.clear);
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+    }
+
+    private void Submit()
+    {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    private void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    private bool Cull()
+    {
+        if(camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+
+        return false;
+    }
+
+    
+}
