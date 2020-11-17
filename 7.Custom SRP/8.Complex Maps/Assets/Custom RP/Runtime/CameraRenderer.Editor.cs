3f3c2bf7787c3ad4f2b82b7a29049d158ba598f4
wjezxujian <wjezxujian@gmail.com>
2020-11-17 14:40:58 +0800

    Add 7.8.Complex Maps: Base Project

---
+++ 7.Custom SRP/8.Complex Maps/Assets/Custom RP/Runtime/CameraRenderer.Editor.cs
@@ -0,0 +1,80 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.Rendering;
+
+partial class CameraRenderer
+{
+    partial void DrawGizmos();
+
+    partial void DrawUnsupportedShaders();
+
+    partial void PrepareForSceneWindow();
+
+    partial void PrepareBuffer();
+
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+    string SampleName { get; set; }
+
+    static ShaderTagId[] legacyShaderTagIds =
+    {
+        new ShaderTagId("Always"),
+        new ShaderTagId("ForwardBase"),
+        new ShaderTagId("PrepassBase"),
+        new ShaderTagId("Vertex"),
+        new ShaderTagId("VertexLMRGBM"),
+        new ShaderTagId("VertexLM")
+    };
+
+    static Material errorMaterial;
+
+    partial void DrawGizmos()
+    {
+        if (Handles.ShouldRenderGizmos())
+        {
+            context.DrawGizmos(camera, GizmoSubset.PreImageEffects);
+            context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+        }
+    }
+
+    partial void DrawUnsupportedShaders()
+    {
+        if (errorMaterial == null)
+        {
+            errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+        }
+
+        DrawingSettings drawingSettings = new DrawingSettings(legacyShaderTagIds[0], new SortingSettings(camera))
+        {
+            overrideMaterial = errorMaterial
+        };
+        for (int i = 1; i < legacyShaderTagIds.Length; ++i)
+        {
+            drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+        }
+        FilteringSettings filteringSettrings = FilteringSettings.defaultValue;
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettrings);
+    }
+
+    partial void PrepareForSceneWindow()
+    {
+        if(camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+    }
+
+    partial void PrepareBuffer()
+    {
+        Profiler.BeginSample("Editor Only");
+        buffer.name = SampleName = camera.name;
+        Profiler.EndSample();
+    }
+
+#else
+
+    //const string SampleName = bufferName;
+    string SampleName => bufferName;
+
+#endif
+}
---
+++ 7.Custom SRP/8.Complex Maps/Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    bool useDynamicBatching, useGPUInstancing;
+
+    CameraRenderer renderer = new CameraRenderer();
+
+    ShadowSettings shadowSettings;
+
+    public CustomRenderPipeline(bool useDynamicBatching, bool useGPUInstancing, bool useSRPBatcher, ShadowSettings shadowSettings)
+    {
+        this.shadowSettings = shadowSettings;
+        this.useDynamicBatching = useDynamicBatching;
+        this.useGPUInstancing = useGPUInstancing;
+
+        GraphicsSettings.useScriptableRenderPipelineBatching = useSRPBatcher;
+        GraphicsSettings.lightsUseLinearIntensity = true;
+    }
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach(Camera camera in cameras)
+        {
+            renderer.Render(context, camera, useDynamicBatching, useGPUInstancing, shadowSettings);
+        }
+    }
+
+    
+}
