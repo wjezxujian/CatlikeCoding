c1b581b2d99521ae7269414b4a0d2ebab119b3d2
wjezxujian <wjezxujian@gmail.com>
2020-11-29 22:28:51 +0800

    Add 7.10.Point and Spot Shadows: Basic Project

---
+++ 7.Custom SRP/10.Point and Spot Shadows/Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CustomRenderPipeline : RenderPipeline
+{
+    bool useDynamicBatching, useGPUInstancing, useLightsPerObject;
+
+    CameraRenderer renderer = new CameraRenderer();
+
+    ShadowSettings shadowSettings;
+
+    public CustomRenderPipeline(bool useDynamicBatching, bool useGPUInstancing, bool useSRPBatcher, bool useLightsPerObject, ShadowSettings shadowSettings)
+    {
+        this.shadowSettings = shadowSettings;
+        this.useDynamicBatching = useDynamicBatching;
+        this.useGPUInstancing = useGPUInstancing;
+        this.useLightsPerObject = useLightsPerObject;
+
+        GraphicsSettings.useScriptableRenderPipelineBatching = useSRPBatcher;
+        GraphicsSettings.lightsUseLinearIntensity = true;
+
+        InitializeForEditor();
+    }
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach(Camera camera in cameras)
+        {
+            renderer.Render(context, camera, useDynamicBatching, useGPUInstancing, useLightsPerObject, shadowSettings);
+        }
+    }
+
+    
+}
