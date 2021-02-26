a92be49d028cc339644644ce696ae781a867ad49
wjezxujian <wjezxujian@gmail.com>
2021-02-26 22:27:01 +0800

    Create 7.12.HDR Project

---
+++ 7.Custom SRP/12.HDR/Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CustomRenderPipeline : RenderPipeline
+{
+    bool useDynamicBatching, useGPUInstancing, useLightsPerObject;
+
+    CameraRenderer renderer = new CameraRenderer();
+
+    ShadowSettings shadowSettings;
+
+    PostFXSettings postFXSettings;
+
+    public CustomRenderPipeline(bool useDynamicBatching, bool useGPUInstancing, bool useSRPBatcher, bool useLightsPerObject, ShadowSettings shadowSettings, PostFXSettings postFXSettings)
+    {
+        this.shadowSettings = shadowSettings;
+        this.useDynamicBatching = useDynamicBatching;
+        this.useGPUInstancing = useGPUInstancing;
+        this.useLightsPerObject = useLightsPerObject;
+        this.postFXSettings = postFXSettings;
+
+        GraphicsSettings.useScriptableRenderPipelineBatching = useSRPBatcher;
+        GraphicsSettings.lightsUseLinearIntensity = true;
+
+        InitializeForEditor();
+    }
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach(Camera camera in cameras)
+        {
+            renderer.Render(context, camera, useDynamicBatching, useGPUInstancing, useLightsPerObject, shadowSettings, postFXSettings);
+        }
+    }
+
+    
+}
